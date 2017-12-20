3ab5dd4016190e778a93e141565e8b1190112b53
asternaut <snooks.sheild@gmail.com>
2017-12-20 10:42:58 -0700

    style updates: top nav, cta, header

--- add-input.js
+++ add-input.js
@@ -13,3 +13,15 @@ function addInput(divName){
 }
 
 $('.svg-inject').svgInject();
+
+
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
